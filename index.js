d625997c943edf3617948e96886d4ce65ce9acd6
Jennifer Gifford <jgifford82@gmail.com>
2022-05-24 19:55:02 -0600

    commit tree 4dd36d8bde87442d1b7391ac341a61f8a38a864d

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jen",
+    streetAddress: "321 Main Street"
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee[key];
+    return employee;
+}
+
+
+// Passes first test, fails second test.
+// function updateEmployeeWithKeyAndValue(obj, key, value) {
+//     const newEmployee = {...employee[key] = value};
+//     return employee;    
+//     return newEmployee;
+// }
+
+// function updateEmployeeWithKeyAndValue(obj, key, value) {
+//     const newEmployee = {...employee[key] = value};    
+//     return newEmployee;
+//     return employee;
+// }
+
+// Passed first test, failed second test. This is destructive to the employee object.
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newEmployee = {...employee[key] = value};
+//     return employee;    
+//     return newEmployee;
+// }
+
+// Failed attempts at 1st test.
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newEmployee = {...employee[key] = value};  
+//     return newEmployee;
+// }
+
+// From the lesson.
+// function nondestructivelyUpdateObject(obj, key, value) {
+//     const newObj = { ...obj };
+  
+//     newObj[key] = value;
+  
+//     return newObj;
+//   }
+  
